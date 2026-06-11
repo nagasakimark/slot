3d663b325600f01e0585805b8ba737c337095839
nagasakimark <m.pook022@g.nagasaki-city.ed.jp>
2026-06-11 18:34:28 +0900

    Add category selector, in-game characters, background image, and new category folders

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const machine = document.querySelector('.machine');
 const prestartScreen = document.querySelector('.prestart-screen');
-const prestartButton = document.querySelector('.prestart-button');
 const coinTrigger = document.querySelector('.coin-trigger');
 const nextRoundButton = document.querySelector('.next-round-button');
 const roundTracker = document.querySelector('.round-tracker');
@@ -23,18 +22,154 @@ spinAudio.preload = 'auto';
 startAudio.preload = 'auto';
 startAudio.loop = true;
 
-const vegetables = [
-    { name: 'Cabbages', japanese: 'キャベツ', src: 'vegetables/cabbages.png' },
-    { name: 'Carrots', japanese: 'にんじん', src: 'vegetables/carrots.png' },
-    { name: 'Corn', japanese: 'とうもろこし', src: 'vegetables/corn.png' },
-    { name: 'Mushrooms', japanese: 'きのこ', src: 'vegetables/mushrooms.png' },
-    { name: 'Onions', japanese: 'たまねぎ', src: 'vegetables/onions.png' },
-    { name: 'Peas', japanese: 'グリーンピース', src: 'vegetables/peas.png' },
-    { name: 'Peppers', japanese: 'ピーマン', src: 'vegetables/peppers.png' },
-    { name: 'Potatoes', japanese: 'じゃがいも', src: 'vegetables/potatoes.png' },
-    { name: 'Pumpkins', japanese: 'かぼちゃ', src: 'vegetables/pumpkins.png' },
-    { name: 'Tomatoes', japanese: 'トマト', src: 'vegetables/tomatoes.png' }
-];
+const categoriesData = {
+    animals: {
+        items: [
+            { name: 'Cats', japanese: 'ねこ', src: 'animals/cats.png' },
+            { name: 'Chickens', japanese: 'にわとり', src: 'animals/chickens.png' },
+            { name: 'Cows', japanese: 'うし', src: 'animals/cows.png' },
+            { name: 'Dogs', japanese: 'いぬ', src: 'animals/dogs.png' },
+            { name: 'Ducks', japanese: 'あひる', src: 'animals/ducks.png' },
+            { name: 'Hamsters', japanese: 'ハムスター', src: 'animals/hamsters.png' },
+            { name: 'Horses', japanese: 'うま', src: 'animals/horses.png' },
+            { name: 'Pigs', japanese: 'ぶた', src: 'animals/pigs.png' },
+            { name: 'Rabbits', japanese: 'うさぎ', src: 'animals/rabbits.png' }
+        ]
+    },
+    animals2: {
+        items: [
+            { name: 'Bear', japanese: 'くま', src: 'animals2/bear.png' },
+            { name: 'Elephant', japanese: 'ぞう', src: 'animals2/elephant.png' },
+            { name: 'Gorilla', japanese: 'ゴリラ', src: 'animals2/gorilla.png' },
+            { name: 'Hippo', japanese: 'カバ', src: 'animals2/hippo.png' },
+            { name: 'Lion', japanese: 'ライオン', src: 'animals2/lion.png' },
+            { name: 'Monkey', japanese: 'さる', src: 'animals2/monkey.png' },
+            { name: 'Panda', japanese: 'パンダ', src: 'animals2/panda.png' },
+            { name: 'Spider', japanese: 'くも', src: 'animals2/spider.png' },
+            { name: 'Tiger', japanese: 'とら', src: 'animals2/tiger.png' },
+            { name: 'Zebra', japanese: 'しまうま', src: 'animals2/zebra.png' }
+        ]
+    },
+    colors: {
+        items: [
+            { name: 'Black', japanese: 'くろ', src: 'colors/black.png' },
+            { name: 'Blue', japanese: 'あお', src: 'colors/blue.png' },
+            { name: 'Brown', japanese: 'ちゃいろ', src: 'colors/brown.png' },
+            { name: 'Gray', japanese: 'はいいろ', src: 'colors/gray.png' },
+            { name: 'Green', japanese: 'みどり', src: 'colors/green.png' },
+            { name: 'Orange', japanese: 'オレンジ', src: 'colors/orange.png' },
+            { name: 'Pink', japanese: 'ピンク', src: 'colors/pink.png' },
+            { name: 'Purple', japanese: 'むらさき', src: 'colors/purple.png' },
+            { name: 'Red', japanese: 'あか', src: 'colors/red.png' },
+            { name: 'White', japanese: 'しろ', src: 'colors/white.png' },
+            { name: 'Yellow', japanese: 'きいろ', src: 'colors/yellow.png' }
+        ]
+    },
+    days: {
+        items: [
+            { name: 'Monday', japanese: 'げつようび', src: 'days/monday.png' },
+            { name: 'Tuesday', japanese: 'かようび', src: 'days/tuesday.png' },
+            { name: 'Wednesday', japanese: 'すいようび', src: 'days/wednesday.png' },
+            { name: 'Thursday', japanese: 'もくようび', src: 'days/thursday.png' },
+            { name: 'Friday', japanese: 'きんようび', src: 'days/friday.png' },
+            { name: 'Saturday', japanese: 'どようび', src: 'days/saturday.png' },
+            { name: 'Sunday', japanese: 'にちようび', src: 'days/sunday.png' }
+        ]
+    },
+    fruits: {
+        items: [
+            { name: 'Apples', japanese: 'りんご', src: 'fruits/apples.png' },
+            { name: 'Bananas', japanese: 'バナナ', src: 'fruits/bananas.png' },
+            { name: 'Cherries', japanese: 'さくらんぼ', src: 'fruits/cherries.png' },
+            { name: 'Grapefruits', japanese: 'グレープフルーツ', src: 'fruits/grapefruits.png' },
+            { name: 'Grapes', japanese: 'ぶどう', src: 'fruits/grapes.png' },
+            { name: 'Oranges', japanese: 'オレンジ', src: 'fruits/oranges.png' },
+            { name: 'Peaches', japanese: 'もも', src: 'fruits/peaches.png' },
+            { name: 'Pears', japanese: 'なし', src: 'fruits/pears.png' },
+            { name: 'Pineapples', japanese: 'パイナップル', src: 'fruits/pineapples.png' }
+        ]
+    },
+    months: {
+        items: [
+            { name: 'January', japanese: 'いちがつ', src: 'months/january.png' },
+            { name: 'February', japanese: 'にがつ', src: 'months/february.png' },
+            { name: 'March', japanese: 'さんがつ', src: 'months/march.png' },
+            { name: 'April', japanese: 'しがつ', src: 'months/april.png' },
+            { name: 'May', japanese: 'ごがつ', src: 'months/may.png' },
+            { name: 'June', japanese: 'ろくがつ', src: 'months/june.png' },
+            { name: 'July', japanese: 'しちがつ', src: 'months/july.png' },
+            { name: 'August', japanese: 'はちがつ', src: 'months/august.png' },
+            { name: 'September', japanese: 'くがつ', src: 'months/september.png' },
+            { name: 'October', japanese: 'じゅうがつ', src: 'months/october.png' },
+            { name: 'November', japanese: 'じゅういちがつ', src: 'months/november.png' },
+            { name: 'December', japanese: 'じゅうにがつ', src: 'months/december.png' }
+        ]
+    },
+    prefectures: {
+        items: [
+            { name: 'Aichi', japanese: 'あいち', src: 'prefectures/aichi.png' },
+            { name: 'Chiba', japanese: 'ちば', src: 'prefectures/chiba.png' },
+            { name: 'Fukuoka', japanese: 'ふくおか', src: 'prefectures/fukuoka.png' },
+            { name: 'Hokkaido', japanese: 'ほっかいどう', src: 'prefectures/hokkaido.png' },
+            { name: 'Hyogo', japanese: 'ひょうご', src: 'prefectures/hyogo.png' },
+            { name: 'Kanagawa', japanese: 'かながわ', src: 'prefectures/kanagawa.png' },
+            { name: 'Osaka', japanese: 'おおさか', src: 'prefectures/osaka.png' },
+            { name: 'Saitama', japanese: 'さいたま', src: 'prefectures/saitama.png' },
+            { name: 'Shizuoka', japanese: 'しずおか', src: 'prefectures/shizuoka.png' },
+            { name: 'Tokyo', japanese: 'とうきょう', src: 'prefectures/tokyo.png' }
+        ]
+    },
+    'sea-animals': {
+        items: [
+            { name: 'Crab', japanese: 'かに', src: 'sea-animals/crab.png' },
+            { name: 'Dolphin', japanese: 'いるか', src: 'sea-animals/dolphin.png' },
+            { name: 'Fish', japanese: 'さかな', src: 'sea-animals/fish.png' },
+            { name: 'Jellyfish', japanese: 'くらげ', src: 'sea-animals/jellyfish.png' },
+            { name: 'Octopus', japanese: 'たこ', src: 'sea-animals/octopus.png' },
+            { name: 'Penguin', japanese: 'ペンギン', src: 'sea-animals/penguin.png' },
+            { name: 'Shark', japanese: 'さめ', src: 'sea-animals/shark.png' },
+            { name: 'Squid', japanese: 'いか', src: 'sea-animals/squid.png' },
+            { name: 'Turtle', japanese: 'かめ', src: 'sea-animals/turtle.png' },
+            { name: 'Whale', japanese: 'くじら', src: 'sea-animals/whale.png' }
+        ]
+    },
+    seasons: {
+        items: [
+            { name: 'Spring', japanese: 'はる', src: 'seasons/spring.png' },
+            { name: 'Summer', japanese: 'なつ', src: 'seasons/summer.png' },
+            { name: 'Autumn', japanese: 'あき', src: 'seasons/autumn.png' },
+            { name: 'Winter', japanese: 'ふゆ', src: 'seasons/winter.png' }
+        ]
+    },
+    sports: {
+        items: [
+            { name: 'Badminton', japanese: 'バドミントン', src: 'sports/badminton.png' },
+            { name: 'Baseball', japanese: 'やきゅう', src: 'sports/baseball.png' },
+            { name: 'Basketball', japanese: 'バスケットボール', src: 'sports/basketball.png' },
+            { name: 'Dodgeball', japanese: 'ドッジボール', src: 'sports/dodgeball.png' },
+            { name: 'Soccer', japanese: 'サッカー', src: 'sports/soccer.png' },
+            { name: 'Table Tennis', japanese: 'たっきゅう', src: 'sports/tabletennis.png' },
+            { name: 'Tennis', japanese: 'テニス', src: 'sports/tennis.png' },
+            { name: 'Volleyball', japanese: 'バレーボール', src: 'sports/volleyball.png' }
+        ]
+    },
+    vegetables: {
+        items: [
+            { name: 'Cabbages', japanese: 'キャベツ', src: 'vegetables/cabbages.png' },
+            { name: 'Carrots', japanese: 'にんじん', src: 'vegetables/carrots.png' },
+            { name: 'Corn', japanese: 'とうもろこし', src: 'vegetables/corn.png' },
+            { name: 'Mushrooms', japanese: 'きのこ', src: 'vegetables/mushrooms.png' },
+            { name: 'Onions', japanese: 'たまねぎ', src: 'vegetables/onions.png' },
+            { name: 'Peas', japanese: 'グリーンピース', src: 'vegetables/peas.png' },
+            { name: 'Peppers', japanese: 'ピーマン', src: 'vegetables/peppers.png' },
+            { name: 'Potatoes', japanese: 'じゃがいも', src: 'vegetables/potatoes.png' },
+            { name: 'Pumpkins', japanese: 'かぼちゃ', src: 'vegetables/pumpkins.png' },
+            { name: 'Tomatoes', japanese: 'トマト', src: 'vegetables/tomatoes.png' }
+        ]
+    }
+};
+
+let vegetables = categoriesData.vegetables.items;
 
 const prizePairs = [
     { id: 1, one: 'prizes/1-1.png', two: 'prizes/1-2.png' },
@@ -194,11 +329,33 @@ function ensureStartAudio() {
     playStartAudio();
 }
 
-function activateStartScreen() {
+function setCategory(categoryKey) {
+    const data = categoriesData[categoryKey];
+    vegetables = data.items;
+
+    vegetables.forEach((item) => {
+        const img = new Image();
+        img.src = item.src;
+    });
+
+    currentReelResults[0] = vegetables[0];
+    currentReelResults[1] = vegetables[Math.min(1, vegetables.length - 1)];
+    phraseVegetableIndex = 0;
+    refillPrizePairs();
+    refillVegetablePairs();
+    prepareRound();
+    populateVocabGrid();
+    updatePhraseCard();
+    renderReelTrack(reelTracks[0], [currentReelResults[0]]);
+    renderReelTrack(reelTracks[1], [currentReelResults[1]]);
+}
+
+function activateStartScreen(categoryKey) {
     if (prestartComplete) {
         return;
     }
 
+    setCategory(categoryKey);
     prestartComplete = true;
     prestartScreen.classList.add('hidden');
     setIntroStage('start');
@@ -239,6 +396,10 @@ function refillVegetablePairs() {
 }
 
 function prepareRound() {
+    const characters = ['toad2.gif', 'mario.webp', 'luigi.gif'];
+    const gameCharacter = document.querySelector('.game-character');
+    gameCharacter.src = characters[Math.floor(Math.random() * characters.length)];
+
     if (availablePrizePairs.length === 0) {
         refillPrizePairs();
     }
@@ -552,6 +713,12 @@ populateVocabGrid();
 updatePhraseCard();
 setIntroStage('start');
 
+const characterImages = ['toad2.gif', 'mario.webp', 'luigi.gif'];
+characterImages.forEach((src) => {
+    const img = new Image();
+    img.src = src;
+});
+
 window.addEventListener('pointerdown', ensureStartAudio, { once: true });
 window.addEventListener('keydown', ensureStartAudio, { once: true });
 
@@ -560,7 +727,9 @@ resultButtons.forEach((button) => {
     button.addEventListener('click', () => revealPrize(button.dataset.side));
 });
 nextRoundButton.addEventListener('click', prepareRound);
-prestartButton.addEventListener('click', activateStartScreen);
+document.querySelectorAll('.category-button').forEach((button) => {
+    button.addEventListener('click', () => activateStartScreen(button.dataset.category));
+});
 startButton.addEventListener('click', async () => {
     if (introStage !== 'start' || !prestartComplete) {
         return;
